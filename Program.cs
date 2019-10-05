6cff20c19ad41cbef57e601cb5d03fafb2ceeb88
Kaue Magid <magidsalem96@gmail.com>
2019-10-05 19:21:34 -0300

    Imprimindo pecas em cores

--- Program.cs
+++ Program.cs
@@ -8,15 +8,21 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            ChessPosition pos = new ChessPosition('a', 1);
-            ChessPosition pos2 = new ChessPosition('c', 3);
-
-            Console.WriteLine(pos);
-            Console.WriteLine(pos2);
-            Console.WriteLine();
-            Console.WriteLine(pos.toPosition());
-            Console.WriteLine(pos2.toPosition());
-
+            try
+            {
+                Board tab = new Board(8, 8);
+                tab.InputPiece(new King(tab, Color.Black), new Position(0, 0));
+                tab.InputPiece(new Queen(tab, Color.Black), new Position(0, 1));
+                tab.InputPiece(new Bishop(tab, Color.Black), new Position(0, 2));
+                tab.InputPiece(new Knight(tab, Color.White), new Position(0, 3));
+                tab.InputPiece(new Tower(tab, Color.Black), new Position(0, 4));
+                tab.InputPiece(new Pawn(tab, Color.White), new Position(0, 5));
+                Screm.printBoard(tab);
+            }
+            catch(BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
